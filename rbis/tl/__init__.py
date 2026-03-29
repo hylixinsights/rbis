4b0dbcdbf5849fa418cb24db9689984f680f4d7f
Helder Nakaya <helder.nakaya@hylix.app>
2026-03-29 18:34:48 -0300

    Expose fidelity and permutation modules in tl API

--- rbis/tl/__init__.py
+++ rbis/tl/__init__.py
@@ -1,11 +1,16 @@
 from .find_markers_sc import find_markers_sc
 from .find_markers_bulk import find_markers_bulk
-from .find_silenced_sc import find_silenced_sc
-from .find_silenced_bulk import find_silenced_bulk
+from .find_silenced_sc import find_silenced_sc, find_silenced_bulk
+from .subcluster_by_signature import subcluster_by_signature
+from .score_cell_fidelity import score_cell_fidelity
+from .permutation import permutation_test
 
 __all__ = [
     "find_markers_sc",
     "find_markers_bulk",
     "find_silenced_sc",
     "find_silenced_bulk",
+    "subcluster_by_signature",
+    "score_cell_fidelity",
+    "permutation_test",
 ]
